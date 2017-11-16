c294923a1b96894e6ca2f128fafe2b0805a5a157
mariouvega <mario.vega014@gmail.com>
2017-11-16 12:24:00 -0500

    Added edit, update, show, and index listing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,15 @@
 class PostsController < ApplicationController
 
+     def index
+          @posts = Post.all
+     end
+
      def new
           @post = Post.new
+     end
+
+     def edit
+          @post = Post.find(params[:id])
      end
 
      def create
@@ -12,11 +20,21 @@ class PostsController < ApplicationController
           else
                render 'new'
           end
+     end
+
+          def update
+               @post =Post.find(params[:id])
+               if @post.update(post_params)
+                    flash[:notice] = "Post was updated and submitted"
+                    redirect_to post_path(@post)
+               else
+                    render 'edit'
+               end
+          end
 
           def show
                @post = Post.find(params[:id])
           end
-     end
 
      private
      def post_params
